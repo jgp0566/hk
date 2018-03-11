3666a155d7c468556d8158f0ec717cbcc877271c
jgp0566 <37267707+jgp0566@users.noreply.github.com>
2018-03-12 07:56:24 +0900

    Delete views.py

--- elections/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Candidate #models에 정의된 Candidate를 import 
-
-def index(request):
-    candidates = Candidate.objects.all()
-    context = {'candidates' : candidates}
-    return render(request, 'elections/index.html', context)
