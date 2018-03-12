2cdd396f56c6193bd715ee47f7c855240e2dd713
jgp0566 <37267707+jgp0566@users.noreply.github.com>
2018-03-12 09:56:21 +0900

    Update models.py

--- transfapp/models.py
+++ transfapp/models.py
@@ -1,15 +1,5 @@
 from django.db import models
-import pandas as pd
 
 class Koreans(models.Model):
     textko = models.TextField()
     textidx = models.IntegerField(default=0)
-    
-    def get_all_news():
-        data = pd.read_csv(r'C:\Users\USER\Desktop\work\HK\scrapping\joongang1.csv', encoding='utf')
-        
-        for i in range(0,data.textko.size):
-            new_text = Koreans(textidx = i, textko = data.textko[i]) 
-            new_text.save() 
-    
-    
